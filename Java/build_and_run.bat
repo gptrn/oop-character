6722df0b6e404e6d34490af57a9357bc58473c44
Flo Sebastian <flo.sebastian@gameloft.com>
2024-08-20 16:10:36 +0700

    - Changed build.bat to build_and_run.bat - Added Java source code of the Character and Main classes

---
+++ Java/build_and_run.bat
@@ -0,0 +1,5 @@
+@echo off
+cd src
+javac Main.java
+java Main
+cd ..
--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,1 +1,23 @@
-empty
+public class Main{
+    public static void main(String[] args){
+        Character balmont = new Character(1, "Balmont", 100, 80, 100, 15);
+        Character grezzle = new Character(2, "Grezzle", 100, 90, 75, 32);
+        
+        balmont.PrintBasicInfo();
+        grezzle.PrintBasicInfo();
+
+        balmont.BasicAttack(grezzle);
+        grezzle.BasicAttack(balmont);
+
+        balmont.PrintBasicInfo();
+        grezzle.PrintBasicInfo();
+
+        balmont.BasicAttack(grezzle);
+        grezzle.BasicAttack(balmont);
+
+        balmont.PrintBasicInfo();
+        grezzle.PrintBasicInfo();
+
+        return;
+    }
+}
